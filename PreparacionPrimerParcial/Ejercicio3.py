757236a20d85f0d0657607f7e9f2d7ead9aff184
AbelSánchez333 <asancheza9@mium.edu.gt>
2024-02-22 13:09:15 -0600

    Se cargaron los programas

---
+++ PreparacionPrimerParcial/Ejercicio3.py
@@ -0,0 +1,43 @@
+#Escriba un programa en Python para verificar si un arbol binario dado es un arbol de busqueda 
+#binaria (BTS) valido o no
+
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+def is_BST(root):
+    stack = []
+    prev = None
+
+    while root or stack:
+        while root:
+            stack. append(root)
+            root = root.left
+        root = stack.pop()
+        if prev and root.val <= prev.val:
+            return False
+        prev = root
+        root = root.right
+    return True
+
+root = TreeNode(2)
+root.left = TreeNode(1)
+root.right = TreeNode(3)
+
+result = is_BST(root)
+print(result)
+
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+
+result = is_BST(root)
+print(result)
+
+
+
+
+
+
